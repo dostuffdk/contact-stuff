7769a5be3114d9bd620481f90919e39e07bdcb3d
Anders Mogensen <anders@dostuff.dk>
2019-02-28 21:32:54 +0100

    Attach template to element.

--- src/contact-stuff.js
+++ src/contact-stuff.js
@@ -1,6 +1,12 @@
+const template = document.createElement('template');
+template.innerHTML = `<div>contact-stuff</div>`;
+
 export default class extends HTMLElement {
 
     constructor() {
         super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 };
