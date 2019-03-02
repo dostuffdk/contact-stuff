fbe88817198d190cffff70aabd297691be0ec82d
Anders Mogensen <anders@dostuff.dk>
2019-03-02 14:30:17 +0100

    Make mailto link if type says so.

--- src/contact-stuff.js
+++ src/contact-stuff.js
@@ -1,10 +1,22 @@
-const template = document.createElement('template');
-template.innerHTML = `<span><slot></slot></span>`;
-
 export default class extends HTMLElement {
+
+    // TODO: Watch for attribute changes.
 
     constructor() {
         super();
+
+        const template = document.createElement('template');
+        const emailContent = '<a href="mailto:{{slot}}"><slot></slot></a>';
+
+        // TODO: If no type is set, make a guess.
+
+        switch (this.getAttribute('type').toUpperCase()) {
+            case 'EMAIL':
+                template.innerHTML = emailContent.replace('{{slot}}', this.innerText);
+                break;
+            default:
+                template.innerHTML = '<slot></slot>';
+        }
 
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
